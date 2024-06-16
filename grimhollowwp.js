1d421657f79b451416572b57254ca18ce22bd933
WolfyFiltiarn <31562992+WolfyFiltiarn@users.noreply.github.com>
2024-06-16 20:00:43 +0200

    Revert to 0.2.1

--- grimhollowwp.js
+++ grimhollowwp.js
@@ -7,24 +7,3 @@ Hooks.on('ready', () => {
 	CONFIG.DND5E.weaponProperties['thrownversatility'] = 'Thrown Vers';
 	
 });
-
-Hooks.once("init", () => {
-  CONFIG.DND5E.weaponIds.Blunderbuss = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.QrRQWXWL1ECa7kA1";
-  CONFIG.DND5E.weaponIds.Braler's Wraps = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.Vqn6S8b10xynkvUO";
-  CONFIG.DND5E.weaponIds.Chakram = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.LuViR0IWJMw9kE4W";
-  CONFIG.DND5E.weaponIds.Claw = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.dOC8ws8dphuguFFK";
-  CONFIG.DND5E.weaponIds.Knuckle Duster = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.2zL3ptVhoMJe4NCy";
-  CONFIG.DND5E.weaponIds.Musket = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.53e8kn4vr9HFDmEK";
-  CONFIG.DND5E.weaponIds.Nunchaku = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.wQCfCTchZrnyzkOX";
-  CONFIG.DND5E.weaponIds.Pistol = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.Dfw8BEsTfFBy6SK3";
-  CONFIG.DND5E.weaponIds.Revolver = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.XRi5aQlJOGsP8dip";
-  CONFIG.DND5E.weaponIds.Rifle = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.TwStP8h1n4ZvXilL";
-  CONFIG.DND5E.weaponIds.Slingshot = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.EgaXZtRXruWtPlUo";
-  CONFIG.DND5E.weaponIds.Spiked Cestus = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.5B1VkMBXBlLcPMgA";
-  CONFIG.DND5E.weaponIds.Spiked Knuckle Duster = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.CRFBRnkoOBpJC4vA";
-  CONFIG.DND5E.weaponIds.Starknife = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.Ls7lmlcOocDvWYgA";
-  CONFIG.DND5E.weaponIds.Tetherhook = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.8FhDtPP6pPDvilRn";
-  CONFIG.DND5E.weaponIds.Twinblades = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.CdeF2Eg0hTCH1ePH";
-  CONFIG.DND5E.weaponIds.War Crescent = "Compendium.larsene-ledger-treasures-and-trinkets.weapons-item.Item.t3FHaqRqftmHCPvR";
-  // etc etc
-});
